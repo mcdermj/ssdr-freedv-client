d52b362ac7734b03a11810ea0ec382a49b703652
Annaliese McDermond <nh6z@nh6z.net>
2020-01-25 23:14:11 -0800

    Reworked the command processing code
    Windows cannot return real file descriptors out of the socket implementation.  This
    makes it less than useful to use for pexpect.  This returns to using pure twisted
    and is probably a better implementation overall.

--- ssdrapiclient.py
+++ ssdrapiclient.py
@@ -1,11 +1,17 @@
-import re
 from twisted.internet.protocol import ClientFactory
 from twisted.protocols.basic import LineOnlyReceiver
-from pexpect.fdpexpect import fdspawn
-from sys import stdout
-from typing import Tuple, List, Optional
+from typing import List
 from twisted.internet import reactor
 from ssdrframe import SsdrFdvClientFrame
+from twisted.internet.defer import Deferred, inlineCallbacks
+import re
+
+
+class CommandFailure(Exception):
+    def __init__(self, errno: int, message: str) -> None:
+        super().__init__()
+        self.errno = errno
+        self.message = message
 
 
 class SsdrApiProtocol(LineOnlyReceiver):
@@ -16,17 +22,11 @@ class SsdrApiProtocol(LineOnlyReceiver):
         self.major_version = 0.0
         self.handle = 0x00
         self.sequence = 0
-        self.vita_port = 0
         self.meters = {}
-        self.major_rev = 0
-        self.minor_rev = 0
-        self.handle = None
-        self.expect = None
-        self.freedv_slice = None
         self.slices = {}
-        self.vita_socket = None
         self.frame = frame
-
+        self.completion_list = {}
+        self.response_matcher = re.compile(r'R([0-9]+)\|([0-9A-Z]{0,8})\|(.*)')
         frame.set_mode_handler(self.mode_handler)
 
     def mode_handler(self, event):
@@ -36,34 +36,48 @@ class SsdrApiProtocol(LineOnlyReceiver):
             if slc['mode'].startswith('FDV'):
                 self.send_command("slice waveform_cmd {} fdv-set-mode={}".format(slcno, mode), wait=False)
 
-    def send_command(self, command: str, wait: bool = True) -> Optional[Tuple[int, str]]:
-        self.expect.sendline('C{}|{}'.format(self.sequence, command))
-        matcher = 'R{}\|([0-9A-Z]{{0,8}})\|(.*)\n'.format(self.sequence)
-        self.sequence = self.sequence + 1
+    def send_command(self, command: str) -> Deferred:
+        self.sendLine('C{}|{}'.format(self.sequence, command).encode('utf-8'))
+        print('Sending: "C{}|{}"'.format(self.sequence, command))
+        d = Deferred()
+        self.completion_list[self.sequence] = d
+        self.sequence += 1
+        return d
 
-        if wait:
-            self.expect.expect(matcher)
-            return int(self.expect.match.group(1)), self.expect.match.group(2)
+    def response_received(self, line: str) -> None:
+        match = self.response_matcher.match(line)
+        print('Recieved response: "{}"'.format(line))
+        if match == None:
+            print("Couldn't parse response line: {}".format(line))
+            return
+
+        sequence = int(match.group(1))
+        errno = int(match.group(2), 16)
+        message = match.group(3)
+
+        if sequence not in self.completion_list:
+            print("Couldn't find sequence {} in completion list".format(sequence))
+            return
+
+        if errno == 0:
+            self.completion_list[sequence].callback(message)
         else:
-            return None
+            self.completion_list[sequence].errback(CommandFailure(errno, message))
 
-    def load_meters(self) -> None:
-        meter_attr_re = re.compile('(\d+)\.nam=([^#]+)')
-
-        (code, message) = self.send_command('meter list')
-        if code != 0:
-            return None  # XXX Exception
+    @inlineCallbacks
+    def load_meters(self) -> Deferred:
+        message = yield self.send_command('meter list')
+        meter_attr_re = re.compile(r'(\d+)\.nam=([^#]+)')
         for match in meter_attr_re.finditer(message):
             self.meters[match.group(2)] = int(match.group(1))
 
-    def subscribe_meters(self, meters: List[str]) -> None:
+    @inlineCallbacks
+    def subscribe_meters(self, meters: List[str]) -> Deferred:
         if len(self.meters) == 0:
-            self.load_meters()
+            yield self.load_meters()
 
         for meter in meters:
-            (code, message) = self.send_command('sub meter {}'.format(self.meters[meter]))
-            if code != 0:
-                print("Error subscribing meter {}({})".format(meter, self.meters[meter]))
+            yield self.send_command('sub meter {}'.format(self.meters[meter]))
 
     def update_meter(self, meter_id: int, meter_value: int):
         for (k, v) in self.meters.items():
@@ -79,24 +93,14 @@ class SsdrApiProtocol(LineOnlyReceiver):
             if slc['mode'].startswith('FDV') and 'fdv-mode' in slc:
                 self.frame.mode = slc['fdv-mode']
 
-    def connectionMade(self) -> None:
+    @inlineCallbacks
+    def connectionMade(self) -> Deferred:
         from vitaprotocol import VitaProtocol
+        vita_socket = yield reactor.listenUDP(0, VitaProtocol(self.transport.getPeer().host, 4993, self))
 
-        self.vita_socket = reactor.listenUDP(0, VitaProtocol(self.transport.getPeer().host, 4993, self))
-        self.vita_port = self.vita_socket.getHost().port
-
-        self.expect = fdspawn(self.transport.getHandle().fileno(), timeout=10, encoding="utf-8", logfile=stdout)
-        self.expect.expect('V(\d+\.\d+)\.(\d+\.\d+)\n')
-        self.major_rev = self.expect.match.group(1)
-        self.minor_rev = self.expect.match.group(2)
-        print('Radio is Version {}/{}'.format(self.major_rev, self.minor_rev))
-
-        self.expect.expect('H([0-9A-F]{8})\n')
-        self.handle = self.expect.match.group(1)
-        print('Our handle is {}'.format(self.handle))
-
-        self.send_command('client udpport {}'.format(self.vita_port), wait=False)
-        self.subscribe_meters([
+        yield self.send_command('client udpport {}'.format(vita_socket.getHost().port))
+        yield self.send_command('sub slice all')
+        yield self.subscribe_meters([
             "fdv-snr",
             "fdv-foff",
             "fdv-total-bits-lsb",
@@ -106,8 +110,6 @@ class SsdrApiProtocol(LineOnlyReceiver):
             "fdv-clock-offset",
             "fdv-sync-quality"
         ])
-
-        self.send_command('sub slice all', wait=False)
 
     def command_received(self, line):
         pass
@@ -140,7 +142,7 @@ class SsdrApiProtocol(LineOnlyReceiver):
         elif line[0] == 'C':
             self.command_received(line)
         elif line[0] == 'R':
-            pass
+            self.response_received(line)
         elif line[0] == 'S':
             self.status_received(line)
         elif line[0] == 'M':
