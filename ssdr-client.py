001aee170ebb93a76d6c036db463b16aba6a61a0
Annaliese McDermond <nh6z@nh6z.net>
2020-01-26 10:37:12 -0800

    Fix pyinstaller incompatibility
    PyInstaller installs its own reactor for some reason when it detects twisted.
    We need to install our own reactor in order to use the proper integration
    with wxWidgets.  This patch removes the installation from PyInstaller to
    correct this.

--- ssdr-client.py
+++ ssdr-client.py
@@ -1,3 +1,10 @@
+#  This is a fix for pyinstaller.  We need to remove the
+#  reactor it's already installed so that we can install
+#  the proper one that works with wxwidgets.
+import sys
+if 'twisted.internet.reactor' in sys.modules:
+    del sys.modules['twisted.internet.reactor']
+
 from twisted.internet import wxreactor
 wxreactor.install()
 
